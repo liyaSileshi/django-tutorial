db47db9c78996b612759d86b334cb63b19ad1940
liyaSileshi <liyasileshi@gmail.com>
2019-10-25 12:48:37 -0700

    part 3

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='polls-index'),
     path('<int:question_id>/', views.detail, name='polls-detail'),
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('pub_date')[:5]
@@ -12,7 +13,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
     
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 
 def result(request, question_id):
     return HttpResponse(f"You're looking at result of question {question_id}")
