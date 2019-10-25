acc2f943ddea11c5505b2b321ef129796c0d81fb
liyaSileshi <liyasileshi@gmail.com>
2019-10-25 11:37:00 -0700

    views and templates

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='polls-index')
+    path('', views.index, name='polls-index'),
+    path('<int:question_id>/', views.detail, name='polls-detail'),
+    path('<int:question_id>/result/', views.result, name='polls-result'),
+    path('<int:question_id>/vote/', views.vote, name='polls-vote')
 ]
 
   
--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
     
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def result(request, question_id):
+    return HttpResponse(f"You're looking at result of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
