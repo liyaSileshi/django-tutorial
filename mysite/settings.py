8dd92d01564f406c4c2680bc9a6fab6768cb1850
liyaSileshi <liyasileshi@gmail.com>
2019-12-03 16:19:27 -0800

    static files added

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'polls.apps.PollsConfig',
-    'pizza'
+    'pizza',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
